5ea864963631ef0dbb5b9d66571c4a081fab7a7e
Jiten Kumar <work.jiten282003@gmail.com>
2025-05-08 23:04:25 +0530

    Update button colors and background in Home component for improved styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,18 +14,18 @@ function handleClickSignUp() {
     }
 
   return (
-    <div className="w-full h-screen flex bg-cover bg-#f0ebd8 text-[0d1321]" >
+    <div className="w-full h-screen flex bg-cover bg-[#FEFAE0]" >
       <div className='w-full flex item-center justify-end mt-10 mr-10'>
 
-        <button onClick={handleClickLogin} className='type-button w-28 h-8 me-2 bg-[#7AE2CF] 
+        <button onClick={handleClickLogin} className='type-button w-28 h-8 me-2 bg-[#606C38] 
           active:scale-[.98] active:duration-75 transition-all hover:scale-[1.1]  ease-in-out transform
           rounded-full align-right border-2 
-          divide-indigo divide-double text-[0d1321] font-bold'>Login</button>
+          border-[#283618] border-double text-white font-bold'>Login</button>
 
-        <button onClick={handleClickSignUp} className='type-button w-28 h-8 mw-2 bg-[#7AE2CF]
+        <button onClick={handleClickSignUp} className='type-button w-28 h-8 mw-2 bg-[#606C38]
           active:scale-[.98] active:duration-75 transition-all hover:scale-[1.1]  ease-in-out transform
           rounded-full align-right border-2 
-          divide-indigo divide-double text-[0d1321] font-bold'>SignUp</button>
+          border-[#283618] border-double text-white font-bold'>SignUp</button>
 
       </div>
     </div>
