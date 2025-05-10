6c7248a3ccd33e8a921a4f7ac1602ed16b35d72a
Jiten Kumar <work.jiten282003@gmail.com>
2025-05-10 23:09:42 +0530

    Update background color for input fields in Login and SignUp components for improved visibility

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -19,25 +19,25 @@ export default function SignUp({
                 <div className='flex flex-col'>
                         <label className='text-lg font-medium'>Username</label>
                         <input
-                            className='w-full bg-[#FEFAE0]/30 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
+                            className='w-full bg-[#FEFAE0] border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
                             type="text" placeholder='Enter your Username' required/>
                     </div>
                     <div className='flex flex-col mt-4'>
                         <label className='text-lg font-medium'>Email</label>
                         <input
-                            className='w-full bg-[#FEFAE0]/30 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
+                            className='w-full bg-[#FEFAE0] border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
                             type={"email"} placeholder="Enter your email"/>
                     </div>
                     <div className='flex flex-col mt-4'>
                     <label className='text-lg font-medium'>Phone Number</label>
                     <input
-                            className='w-full bg-[#FEFAE0]/30 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
+                            className='w-full bg-[#FEFAE0] border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
                             type="tel" placeholder='Enter your Phone Number' maxLength={10} required />
                     </div>
                     <div className='flex flex-col mt-4'>
                         <label className='text-lg font-medium'>Password</label>
                         <input
-                            className='w-full bg-[#FEFAE0]/30 border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
+                            className='w-full bg-[#FEFAE0] border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
                             placeholder="Enter your email"
                             type={"password"}
                         />
@@ -45,7 +45,7 @@ export default function SignUp({
                     <div className='mt-8 flex flex-col gap-y-4'>
                         <button className='active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out transform py-4 bg-[#FCA311] rounded-xl text-[#03045e] font-bold text-lg border-2 border-yellow-400 '>Sign up</button>
                         <button
-                            className='flex items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out transform py-4 rounded-xl text-gray-700 font-semibold text-lg bg-[#FEFAE0]/80 border-2 border-gray-100 border-yellow-400'>
+                            className='flex items-center justify-center gap-2 active:scale-[.98] active:duration-75 transition-all hover:scale-[1.01] ease-in-out transform py-4 rounded-xl text-gray-700 font-semibold text-lg bg-[#FEFAE0]/70 border-2 border-gray-100 border-yellow-400'>
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M5.26644 9.76453C6.19903 6.93863 8.85469 4.90909 12.0002 4.90909C13.6912 4.90909 15.2184 5.50909 16.4184 6.49091L19.9093 3C17.7821 1.14545 15.0548 0 12.0002 0C7.27031 0 3.19799 2.6983 1.24023 6.65002L5.26644 9.76453Z" fill="#EA4335"/>
                                     <path d="M16.0406 18.0142C14.9508 18.718 13.5659 19.0926 11.9998 19.0926C8.86633 19.0926 6.21896 17.0785 5.27682 14.2695L1.2373 17.3366C3.19263 21.2953 7.26484 24.0017 11.9998 24.0017C14.9327 24.0017 17.7352 22.959 19.834 21.0012L16.0406 18.0142Z" fill="#34A853"/>
